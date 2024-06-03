711cff58fa1e814724fab97c38466f0317a2e16f
ximeen <gabriximenes100@gmail.com>
2024-06-03 10:52:16 -0300

    :up: remove description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,19 +10,17 @@ export default function Home() {
       <header
         className="flex flex-col justify-center items-center pt-10">
         <Image
-        width={200}
-        height={200}
+        width={500}
+        height={500}
           src="/profile.png"
           alt="profile image"
-          className="h-auto w-52 bg-no-repeat bg-cover" />
+          className="h-auto w-80 bg-no-repeat bg-cover" />
         <section className="mt-10 flex flex-col gap-2 text-center items-center">
           <div className="flex gap-2 items-center">
             <h3 className="lg:text-2xl text-xl font-semibold text-zinc-800">Oren Refrigeração Automotiva</h3>
             <Image width={20} height={20} className="h-7 w-7 " src="/verify.png" alt="Icon Verify" />
           </div>
-          <div className="flex justify-center max-w-sm lg:max-w-md">
-            <span className="text-center text-sm lg:text-base text-zinc-700">Distribuidor Autorizado AC automotivo / Denso / Elietch Brasil / Mahle metal leve S.A. / Pescan Químicos / Phinia Delphi</span>
-          </div>
+          
         </section>
       </header>
 
@@ -39,12 +37,12 @@ export default function Home() {
             icon={()=> <WhatsappLogo width={30} height={30}  className="mr-2"/> }
           />
            <ProductLinks
-            Name='Localização Imperatriz-MA'
+            Name='Matriz Imperatriz-MA'
             href='https://www.google.com/maps/place/OREN+REFRIGERA%C3%87%C3%83O+-+AR+CONDICIONADO+AUTOMOTIVA/@-5.5198865,-47.4639448,15z/data=!4m6!3m5!1s0x92c55fae6c343761:0xff6402f9a0a077ae!8m2!3d-5.5198865!4d-47.4639448!16s%2Fg%2F11cs6jw360?entry=ttu'
             icon={()=> <MapPin width={30} height={30}  className="mr-2"/>}
           />
            <ProductLinks
-            Name='Localização Balsas-MA'
+            Name='Filial Balsas-MA'
             href='https://www.google.com.br/maps/place/Oren+Refrigera%C3%A7%C3%A3o+Automotiva/@-7.5148568,-46.0389255,17z/data=!4m6!3m5!1s0x92d5ef16b908309f:0xcd30bfb63cde1738!8m2!3d-7.5149332!4d-46.0388812!16s%2Fg%2F11r7sf7092?entry=ttu'
             icon={()=> <MapPin width={30} height={30}  className="mr-2"/>}
           />
